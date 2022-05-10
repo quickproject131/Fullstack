ed2a31b4ff49e0e294fad8fe4b5e2a4289e9dd70
quickproject131 <104439631+quickproject131@users.noreply.github.com>
2022-05-10 23:25:44 +0530

    dcsd

--- BackendApp/src/main/java/com/backend/DBUtil/Connection.java
+++ BackendApp/src/main/java/com/backend/DBUtil/Connection.java
@@ -1,38 +1,32 @@
 package com.backend.DBUtil;
 
+
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Connection {
-
 	private static Connection connection = null;
-	
 	public static Connection getConnection() {
-		if(connection!=null)
-		{
+		if (connection != null)
 			return connection;
-		}else
-		{
+		else {
+
 			try {
-				String driver = "com.mysql.jdbc.Driver";
-				String url = "jdbc:mysql://localhost:3306/college";
-				String user = "root";
-				String password = "123456789";
-				
+				String driver="com.mysql.cj.jdbc.Driver";
+				String url="jdbc:mysql://localhost:3306/college?useSSL=false";
+				String user="root";
+				String password="123456789";
 				Class.forName(driver);
-			    connection = (Connection) DriverManager.getConnection(url,user,password);
+				connection=(Connection) DriverManager.getConnection(url,user,password);
 			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
+			} 
+			return connection;
 		}
-		
-		
-		return connection;
+
 	}
  
 }
